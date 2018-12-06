57ccfc1658f21123a613ca1ff37c10ceb32dbe22
d-kezuka <d-kezuka@e-reverse.com>
2018-12-06 18:03:56 +0900

    New file.

---
+++ MvcTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // Get:/JelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Get:/HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MvcTutorial/Data/MvcTutorialContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcTutorial.Models
+{
+    public class MvcTutorialContext : DbContext
+    {
+        public MvcTutorialContext (DbContextOptions<MvcTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcTutorial.Models.Movie> Movie { get; set; }
+    }
+}
